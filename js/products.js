d0901b4e7c3ace4904f6af759a349d9510c47858
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-03-14 22:56:35 +0100

    indented the code

--- js/products.js
+++ js/products.js
@@ -9,14 +9,14 @@ const showProducts = () => {
 
   jackets.forEach((jacket) => {
     container.innerHTML += `<a class="a-product-card" href="/html/details.html?id=${jacket.id}">
-    <div class="product-card">
-      <img class="product-img" src="${jacket.image}" alt="${jacket.name}"/>
-      <div class="product-text">
-        <p class="name">${jacket.name} <i class="fa-solid fa-heart heart"></i></p>
-        <p class="price">${jacket.price} €</p>
-      </div>
-    </div>
-  </a>`;
+                              <div class="product-card">
+                                <img class="product-img" src="${jacket.image}" alt="${jacket.name}"/>
+                                <div class="product-text">
+                                  <p class="name">${jacket.name} <i class="fa-solid fa-heart heart"></i></p>
+                                  <p class="price">${jacket.price} €</p>
+                                </div>
+                              </div>
+                            </a>`;
   });
 };
 
