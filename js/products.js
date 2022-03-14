bffb1ffd6280f2d4f765874dd79f4b257846a13f
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-03-14 22:52:41 +0100

    corrected the comment

--- js/products.js
+++ js/products.js
@@ -25,12 +25,12 @@ showProducts();
 /* THIS PART I WISH TO BEHOLD FOR MYSELF:
 
 jackets.forEach((jackets) => {
-  html += `<a class="a-product-card" href="/html/details.html?id=${jackets.id}">
+  html += `<a class="a-product-card" href="/html/details.html?id=${jacket.id}">
             <div class="product-card">
-              <img class="product-img" src="${jackets.image}"/>
+              <img class="product-img" src="${jacket.image}"/>
               <div class="product-text">
-                <p class="name">${jackets.name}<i class="fa-solid fa-heart heart"></i></p>
-                <p class="price">${jackets.price}</p>
+                <p class="name">${jacket.name}<i class="fa-solid fa-heart heart"></i></p>
+                <p class="price">${jacket.price}</p>
               </div>
             </div>
           </a>`;
