d6882d8f945d3a51d7d6301aff76439156d06ccc
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-03-15 21:44:54 +0100

    corrected the prices of the jackets

--- js/array.js
+++ js/array.js
@@ -2,7 +2,7 @@ export const jackets = [
   {
     id: "0",
     name: "Wylie Snowboard Jacket",
-    price: "112 €",
+    price: "112",
     description:
       "Dope's very own signature style which stays in the range to reign supreme over all other anoraks. We gave it waterproofing and breathability to tackle everything the mountain has on offer, and lots of slick little details like our contrast drawstrings and triple pocket system. Which makes it one serious street-style inspired anorak that’s ready to slash everything from the park to the pow, and look awesome doing it.",
     image: "../images/products/E6145_01_7CmeNwj.avif",
@@ -10,7 +10,7 @@ export const jackets = [
   {
     id: "1",
     name: "Wylie Snowboard Jacket",
-    price: "125 €",
+    price: "125",
     description:
       "Dope's very own signature style which stays in the range to reign supreme over all other anoraks. We gave it waterproofing and breathability to tackle everything the mountain has on offer, and lots of slick little details like our contrast drawstrings and triple pocket system. Which makes it one serious street-style inspired anorak that’s ready to slash everything from the park to the pow, and look awesome doing it.",
     image: "../images/products/E6208_01_88ZPuJr.avif",
@@ -18,7 +18,7 @@ export const jackets = [
   {
     id: "2",
     name: "Annok Snowboard Jacket",
-    price: "158 €",
+    price: "158",
     description:
       "Dope's very own signature style which stays in the range to reign supreme over all other anoraks. We gave it waterproofing and breathability to tackle everything the mountain has on offer, and lots of slick little details like our contrast drawstrings and triple pocket system. Which makes it one serious street-style inspired anorak that’s ready to slash everything from the park to the pow, and look awesome doing it.",
     image: "../images/products/E8180_01_77cmCWY.avif",
@@ -26,7 +26,7 @@ export const jackets = [
   {
     id: "3",
     name: "Blizzard Snowboard Jacket",
-    price: "133 €",
+    price: "133",
     description:
       "Dope's very own signature style which stays in the range to reign supreme over all other anoraks. We gave it waterproofing and breathability to tackle everything the mountain has on offer, and lots of slick little details like our contrast drawstrings and triple pocket system. Which makes it one serious street-style inspired anorak that’s ready to slash everything from the park to the pow, and look awesome doing it.",
     image: "../images/products/E8181_01_scZYFxX.avif",
@@ -35,7 +35,7 @@ export const jackets = [
   {
     id: "4",
     name: "Annok Snowboard Jacket",
-    price: "158 €",
+    price: "158",
     description:
       "Dope's very own signature style which stays in the range to reign supreme over all other anoraks. We gave it waterproofing and breathability to tackle everything the mountain has on offer, and lots of slick little details like our contrast drawstrings and triple pocket system. Which makes it one serious street-style inspired anorak that’s ready to slash everything from the park to the pow, and look awesome doing it.",
     image: "../images/products/E8392_01_dSRMGR5.avif",
@@ -43,7 +43,7 @@ export const jackets = [
   {
     id: "5",
     name: "Adept Snowboard Jacket",
-    price: "160 €",
+    price: "160",
     description:
       "Dope's very own signature style which stays in the range to reign supreme over all other anoraks. We gave it waterproofing and breathability to tackle everything the mountain has on offer, and lots of slick little details like our contrast drawstrings and triple pocket system. Which makes it one serious street-style inspired anorak that’s ready to slash everything from the park to the pow, and look awesome doing it.",
     image: "../images/products/E8424_200.avif",
@@ -51,7 +51,7 @@ export const jackets = [
   {
     id: "6",
     name: "Legacy Snowboard Jacket",
-    price: "135 €",
+    price: "135",
     description:
       "Dope's very own signature style which stays in the range to reign supreme over all other anoraks. We gave it waterproofing and breathability to tackle everything the mountain has on offer, and lots of slick little details like our contrast drawstrings and triple pocket system. Which makes it one serious street-style inspired anorak that’s ready to slash everything from the park to the pow, and look awesome doing it.",
     image: "../images/products/F0450_01_k5LI2j9.avif",
@@ -59,7 +59,7 @@ export const jackets = [
   {
     id: "7",
     name: "Cyclone Snowboard Jacket",
-    price: "140 €",
+    price: "140",
     description:
       "Dope's very own signature style which stays in the range to reign supreme over all other anoraks. We gave it waterproofing and breathability to tackle everything the mountain has on offer, and lots of slick little details like our contrast drawstrings and triple pocket system. Which makes it one serious street-style inspired anorak that’s ready to slash everything from the park to the pow, and look awesome doing it.",
     image: "../images/products/F0618_01_l1FWanm.avif",
@@ -68,7 +68,7 @@ export const jackets = [
   {
     id: "8",
     name: "Akin Snowboard Jacket",
-    price: "200 €",
+    price: "200",
     description:
       "Dope's very own signature style which stays in the range to reign supreme over all other anoraks. We gave it waterproofing and breathability to tackle everything the mountain has on offer, and lots of slick little details like our contrast drawstrings and triple pocket system. Which makes it one serious street-style inspired anorak that’s ready to slash everything from the park to the pow, and look awesome doing it.",
     image: "../images/products/F0645_01_TpgLizB.avif",
@@ -76,7 +76,7 @@ export const jackets = [
   {
     id: "9",
     name: "Yeti Snowboard Jacket",
-    price: "117 €",
+    price: "117",
     description:
       "Dope's very own signature style which stays in the range to reign supreme over all other anoraks. We gave it waterproofing and breathability to tackle everything the mountain has on offer, and lots of slick little details like our contrast drawstrings and triple pocket system. Which makes it one serious street-style inspired anorak that’s ready to slash everything from the park to the pow, and look awesome doing it.",
     image: "../images/products/F0648_01_rW6emoF.avif",
@@ -84,7 +84,7 @@ export const jackets = [
   {
     id: "10",
     name: "Insulated Midlayer Jacket",
-    price: "92 €",
+    price: "92",
     description:
       "Dope's very own signature style which stays in the range to reign supreme over all other anoraks. We gave it waterproofing and breathability to tackle everything the mountain has on offer, and lots of slick little details like our contrast drawstrings and triple pocket system. Which makes it one serious street-style inspired anorak that’s ready to slash everything from the park to the pow, and look awesome doing it.",
     image: "../images/products/F0657_01_KqfF2Dt.avif",
@@ -92,7 +92,7 @@ export const jackets = [
   {
     id: "11",
     name: "Blizzard Snowboard Jacket",
-    price: "158 €",
+    price: "158",
     description:
       "Dope's very own signature style which stays in the range to reign supreme over all other anoraks. We gave it waterproofing and breathability to tackle everything the mountain has on offer, and lots of slick little details like our contrast drawstrings and triple pocket system. Which makes it one serious street-style inspired anorak that’s ready to slash everything from the park to the pow, and look awesome doing it.",
     image: "../images/products/H0223_01_KtNwDH0.avif",
