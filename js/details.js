8242b46b47c4870c84971cf8df60611649b19b6d
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-03-15 13:53:32 +0100

    removed <a> link from ja file to html file

--- js/details.js
+++ js/details.js
@@ -12,8 +12,7 @@ const id = params.get("id");
 const showDetails = () => {
   jackets.forEach((jacket) => {
     if (jacket.id == id) {
-      html += `<a href="../html/products.html" class="breadcrumb-nav"> << Back to products</a>
-              <img class="image-jacket" src="${jacket.image}" alt="${jacket.name}">
+      html += `<img class="image-jacket" src="${jacket.image}" alt="${jacket.name}">
               <div class="container-text">
                 <div class="container-text-small">
                   <h1 class="name">${jacket.name}</h1>
