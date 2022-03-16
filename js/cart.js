368941ab6da947d154e51f1f362703f1c4a055d1
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-03-16 23:02:15 +0100

    trying to create cart

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,31 @@
-import { getExistingJacketsList } from "./components/jacketsFunction";
+import { getExistingJacketsList } from "./components/jacketsFunction.js";
 
 const jackets = getExistingJacketsList();
 
-console.log(jackets);
+const containerCart = document.querySelector(".container-cart");
+
+const createCart = () => {
+  jackets.forEach((jacket) => {
+    if (jackets.length >= 1) {
+      html += `<div class="container-row">
+                <h3 class="modal-title" id="exampleModalLabel">Your Cart</h3>
+                <div>€<span class="total-cart"></span></div>
+              </div>
+              <div class="container-row">
+                <img src="#" alt="">
+                <h4 class="cart-jackets-name"></h4>
+              </div>
+              <div class="container-row">
+                <button type="button" class="btn">Go to Checkout</button>
+              </div>`;
+    } else {
+      containerCart.innerHTML = "Your cart is empty.";
+    }
+  });
+  containerCart.innerHTML = html;
+};
+
+createCart();
 
 /*
 // Show/Hide cart
