314a5451fb52ed696964dcc7ccc67ad0a213eda2
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-03-16 18:33:29 +0100

    created the file

---
+++ js/cart.js
@@ -0,0 +1,7 @@
+// ************************************************
+// Shopping Cart API
+// ************************************************
+
+// Show/Hide cart
+
+const buttonAddToCart = document.querySelector(".add-to-cart");
