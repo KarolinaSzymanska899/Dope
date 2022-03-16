9b908e1150d1514107b45c36ae79fafd02bde4ea
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-03-16 22:16:21 +0100

    beginning to write a function

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,15 @@
-// ************************************************
-// Shopping Cart API
-// ************************************************
+import { getExistingJacketsList } from "./components/jacketsFunction";
 
+const jackets = getExistingJacketsList();
+
+console.log(jackets);
+
+/*
 // Show/Hide cart
 
 const buttonAddToCart = document.querySelector(".add-to-cart");
+
+buttonAddToCart.addEventListener("click", function (){
+
+})
+*/
