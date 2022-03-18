6cee9a212d4e4dfdae7b7a630755369100a97609
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-03-18 01:15:50 +0100

    trying to make cart

--- js/cart.js
+++ js/cart.js
@@ -4,7 +4,32 @@ const jackets = getExistingJacketsList();
 
 const containerCart = document.querySelector(".container-cart");
 
-const createCart = () => {
+if (jackets.length === 0) {
+  containerCart.innerHTML = "Your cart is empty";
+}
+
+jackets.forEach((jacket) => {
+  containerCart.innerHTML += `<div class="container-row">
+                                <h3 class="modal-title" id="exampleModalLabel">Your Cart</h3>
+                                <div>€<span class="total-cart"></span></div>
+                              </div>
+                              <div class="container-row">
+                                <img src="#" alt="">
+                                <h4 class="cart-jackets-name"></h4>
+                              </div>
+                              <div class="container-row">
+                                <button type="button" class="btn">Go to Checkout</button>
+                              </div>`;
+});
+
+// Show cart
+const buttonAddToCart = document.querySelector(".add-to-cart");
+
+buttonAddToCart.addEventListener("click", function () {
+  containerCart.style.display = "flex";
+});
+
+/*const createCart = () => {
   jackets.forEach((jacket) => {
     if (jackets.length >= 1) {
       html += `<div class="container-row">
@@ -25,14 +50,4 @@ const createCart = () => {
   containerCart.innerHTML = html;
 };
 
-createCart();
-
-/*
-// Show/Hide cart
-
-const buttonAddToCart = document.querySelector(".add-to-cart");
-
-buttonAddToCart.addEventListener("click", function (){
-
-})
-*/
+createCart();*/
